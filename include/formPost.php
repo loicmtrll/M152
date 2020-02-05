4935f548101309f2222bb6314ee2e3eb142b7f21
unknown <loic.mtrll@eduge.ch>
2020-02-05 12:44:12 +0100

    test

---
+++ include/formPost.php
@@ -0,0 +1,17 @@
+
+
+<div class="container">
+  <form action="/action_page.php">
+
+    <label for="">Description</label>
+    <textarea required rows="4" cols="50" style="height:250px;resize: none;">
+    </textarea>
+
+    <label for="img">Fichier image</label><br>
+    <input type="file" name="img" accept="image/*" required>
+
+
+    <br><br><br>
+    <input type="submit" value="Submit">
+  </form>
+</div>
---
+++ post.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<title>Page principale</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<link rel="stylesheet" href="./css/formPost.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+
+</style>
+<body>
+
+
+
+<!-- Top menu -->
+<div class="w3-top">
+  <div class="w3-white w3-xlarge" style="max-width:1200px;margin:auto">
+    <div class="w3-right w3-padding-16">Mon portefolio</div>
+    <div class="w3-center w3-padding-16"><a href="index.php">Home</a></div>
+  </div>
+</div>
+  
+<!-- !PAGE CONTENT! -->
+<hr>
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+
+
+  <hr>
+
+    <?php 
+        include "include/formPost.php";
+    ?>
+  
+  <hr id="about">
+
+ 
+  
+  <!-- Footer -->
+  <footer class="w3-row-padding w3-padding-32">
+    <div class="w3-third">
+      <h3>FOOTER</h3>
+      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    </div>  
+  </footer>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+</body>
+</html>
